02456807f360b8c7305ba65d65e160ec0a6d92ae
virsworld <virpatel71@gmail.com>
2025-09-19 04:52:46 -0400

    dynamically loading first project.................

--- projects.js
+++ projects.js
@@ -1,142 +1,111 @@
 const projects = [
-        `
-            <div class="card medium">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img alt="music streaming app" src="/assets/img/gis_cover.png" style="height: 100%; width: 100%" class="activator" />
-              </div>
-              <div class="card-content">
-                <span class="card-title activator teal-text hoverline">Ene GIS<i
-                    class="mdi-navigation-more-vert right"></i></span>
-                <p>
-                  Environmentally conscious Geographic Information System (GIS).
-                </p>
-              </div>
-              <div class="card-reveal">
-                <!-- TODO: change this -- only close button -->
-                <!-- <span class="card-title brown-text">Accomplishments<i class="mdi-navigation-close right"></i></span> -->
-                <span class="card-title grey-text"><small>Accomplishments</small><i
-                    class="mdi-navigation-close right"></i></span>
-                <ul>
-                  <li><b>Tools:</b> C++</li>
-                  <li>Generate directions to and from places in popular cities.</li>
-                  <li>Access bike share locations available in urban cities</li>
-                  <li>Access electric vehicle charging locations</li>
-                </ul>
-                <div class="card-action">
-                  <a aria-label="Visit " href="https://www.linkedin.com/in/vir-patel/overlay/1755130631501/single-media-viewer/?type=DOCUMENT&profileId=ACoAAEms4UMBDTOFNgpzjIul5UyZPgpDvqY4u0E" target="_blank" data-position="top"
-                    data-tooltip="View Online"
-                    class="btn-floating btn-large waves-effect waves-light blue-grey tooltipped"><i
-                      class="fa fa-external-link"></i></a>
-                </div>
-              </div>
-            </div>
-        `,
-        `
-            <div class="card medium">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img alt="quiz app" src="/assets/img/cracker_project.png" style="height: 100%; width: 100%" class="activator" />
-              </div>
-              <div class="card-content">
-                <span class="card-title activator teal-text hoverline">Password Cracker<i
-                    class="mdi-navigation-more-vert right"></i></span>
-                <p>
-                  Attempts to crack user passwords taken from leaked hash data by hashing common passwords and cross-referencing.
-                </p>
-              </div>
-              <div class="card-reveal">
-                <!-- TODO: change this -- only close button -->
-                <!-- <span class="card-title brown-text">Accomplishments<i class="mdi-navigation-close right"></i></span> -->
-                <span class="card-title grey-text"><small>Accomplishments</small><i
-                    class="mdi-navigation-close right"></i></span>
-                <ul>
-                  <li><b>Tools:</b> Verilog, ModelSim</li>
-                  <li>Reveal the password of users affected by a data leak.</li>
-                  <li>Two modes avaialbe, all and target.</li>
+  {
+    img: "/assets/img/gis_cover.png",
+    alt: "music streaming app",
+    title: "Ene GIS",
+    description: "Environmentally conscious Geographic Information System (GIS).",
+    tools: "C++",
+    accomplishments: [
+      "Generate directions to and from places in popular cities.",
+      "Access bike share locations available in urban cities",
+      "Access electric vehicle charging locations"
+    ],
+    link: "https://www.linkedin.com/in/vir-patel/overlay/1755130631501/single-media-viewer/?type=DOCUMENT&profileId=ACoAAEms4UMBDTOFNgpzjIul5UyZPgpDvqY4u0E"
+  },
+  {
+    img: "/assets/img/cracker_project.png",
+    alt: "quiz app",
+    title: "Password Cracker",
+    description: "Attempts to crack user passwords taken from leaked hash data by hashing common passwords and cross-referencing.",
+    tools: "Verilog, ModelSim",
+    accomplishments: [
+      "Reveal the password of users affected by a data leak.",
+      "Two modes avaialbe, all and target."
+    ],
+    link: "https://docs.google.com/presentation/d/1Uy7J0Y0WWg2kw5RO47jo83wm7phxgxnKt66pGZyQ9IQ/edit?slide=id.g3258c218dd2_0_0#slide=id.g3258c218dd2_0_0"
+  },
+  {
+    img: "/assets/img/tcdb.png",
+    alt: "quiz app",
+    title: "TestcaseDB",
+    description: "Made for programming students to ensure their program can handle more than the public test cases available. Students should create their own test cases but use this as a resource to check out what their peers have come up with and share their testing. I plan on continuing to add more features for fun :D (like editing the uploaded test cases)",
+    tools: "React.js, JavaScript, Node.js",
+    accomplishments: [
+      "Upload/delete test cases.",
+      "View other peoples test cases"
+    ],
+    link: "https://testcasedb.netlify.app"
+  }
+];
 
-                </ul>
-                <div class="card-action">
-                  <a aria-label="Visit " href="https://docs.google.com/presentation/d/1Uy7J0Y0WWg2kw5RO47jo83wm7phxgxnKt66pGZyQ9IQ/edit?slide=id.g3258c218dd2_0_0#slide=id.g3258c218dd2_0_0" target="_blank" data-position="top"
-                    data-tooltip="View Online"
-                    class="btn-floating btn-large waves-effect waves-light blue-grey tooltipped"><i
-                      class="fa fa-external-link"></i></a>
-                </div>
-              </div>
-            </div>
-        `,
-        `
-            <div class="card medium">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img alt="quiz app" src="/assets/img/tcdb.png" style="height: 100%; width: 100%" class="activator" />
-              </div>
-              <div class="card-content">
-                <span class="card-title activator teal-text hoverline">TestcaseDB<i
-                    class="mdi-navigation-more-vert right"></i></span>
-                <p>
-                  Made for programming students to ensure their program can handle more than the public test cases available. Students should create their own test cases but use this as a resource to check out what their peers have come up with and share their testing. I plan on continuing to add more features for fun :D (like editing the uploaded test cases)
-                </p>
-              </div>
-              <div class="card-reveal">
-                <!-- TODO: change this -- only close button -->
-                <!-- <span class="card-title brown-text">Accomplishments<i class="mdi-navigation-close right"></i></span> -->
-                <span class="card-title grey-text"><small>Accomplishments</small><i
-                    class="mdi-navigation-close right"></i></span>
-                <ul>
-                  <li><b>Tools:</b> React.js, JavaScript, Node.js</li>
-                  <li>Upload/delete test cases.</li>
-                  <li>View other peoples test cases</li>
-
-                </ul>
-                <div class="card-action">
-                  <a aria-label="Visit " href="https://testcasedb.netlify.app" target="_blank" data-position="top"
-                    data-tooltip="View Online"
-                    class="btn-floating btn-large waves-effect waves-light blue-grey tooltipped"><i
-                      class="fa fa-external-link"></i></a>
-                </div>
-              </div>
-            </div>
-        `];
+function buildProjectCard(project) {
+  const projDiv = document.createElement("div");
+  projDiv.classList = "col s12 m6 l4";
+  projDiv.innerHTML = `
+    <div class="card medium">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img alt="${project.alt}" src="${project.img}" style="height: 100%; width: 100%" class="activator" />
+      </div>
+      <div class="card-content">
+        <span class="card-title activator teal-text hoverline">${project.title}<i
+            class="mdi-navigation-more-vert right"></i></span>
+        <p>${project.description}</p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text"><small>Accomplishments</small><i
+            class="mdi-navigation-close right"></i></span>
+        <ul>
+          <li><b>Tools:</b> ${project.tools}</li>
+          ${project.accomplishments.map(acc => `<li>${acc}</li>`).join("")}
+        </ul>
+        <div class="card-action">
+          <a aria-label="Visit " href="${project.link}" target="_blank" data-position="top"
+            data-tooltip="View Online"
+            class="btn-floating btn-large waves-effect waves-light blue-grey tooltipped"><i
+              class="fa fa-external-link"></i></a>
+        </div>
+      </div>
+    </div>
+  `;
+  return projDiv;
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-    const projects_section = document.getElementById('projects');
-    const title = document.createElement('h3');
-    const container = document.createElement('div');
-    const row = document.createElement('div');
-    const project = document.createElement('div');
-    
-    title.classList = "page-title white-text teal";
-    title.innerText = "Recent Projects";
-    projects_section.appendChild(title);
-    projects_section.appendChild(container);
-    container.appendChild(row);
-    project.classList = "col s12 m6 l4";
-    project.innerHTML = projects[0];
-    row.classList = "row";
-    row.appendChild(project);
+  const projects_section = document.getElementById('projects');
+  const title = document.createElement('h3');
+  const container = document.createElement('div');
+  const row = document.createElement('div');
 
-    const more_btn_col = document.createElement('div');
-    more_btn_col.classList = "col s12 m6 l4";
-    const more_btn_card = document.createElement('div');
-    more_btn_card.classList = "card medium center-align";
+  title.classList = "page-title white-text teal";
+  title.innerText = "Recent Projects";
+  projects_section.appendChild(title);
+  projects_section.appendChild(container);
+  container.appendChild(row);
+  row.classList = "row";
 
-    const more_btn = document.createElement('button');
-    more_btn.style.width = "100%";
-    more_btn.style.height = "100%";
-    more_btn.innerText = "Load More";
-    more_btn.classList = "btn waves-effect waves-light teal";
+  // first project
+  row.appendChild(buildProjectCard(projects[0]));
 
-    more_btn_card.appendChild(more_btn);
-    more_btn_col.appendChild(more_btn_card);
-    row.appendChild(more_btn_col);
-    
-    more_btn.onclick = () => {
-        projects.forEach((element, i) => {
-            if (i) {
-                const projDiv = document.createElement('div');
-                projDiv.classList = "col s12 m6 l4";
-                projDiv.innerHTML = element;
-                row.appendChild(projDiv);
-            }
-        });
-        more_btn_col.style.display = 'none';
-    }
+  // load more button
+  const more_btn_col = document.createElement('div');
+  more_btn_col.classList = "col s12 m6 l4";
+  const more_btn_card = document.createElement('div');
+  more_btn_card.classList = "card medium center-align";
+
+  const more_btn = document.createElement('button');
+  more_btn.style.width = "100%";
+  more_btn.style.height = "100%";
+  more_btn.innerText = "Load More";
+  more_btn.classList = "btn waves-effect waves-light teal";
+
+  more_btn_card.appendChild(more_btn);
+  more_btn_col.appendChild(more_btn_card);
+  row.appendChild(more_btn_col);
+
+  more_btn.onclick = () => {
+    projects.forEach((project, i) => {
+      if (i) row.appendChild(buildProjectCard(project));
+    });
+    more_btn_col.style.display = 'none';
+  };
 });
